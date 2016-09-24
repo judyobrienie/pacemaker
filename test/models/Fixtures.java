5fb26e1bff45e27785497c81e178eaac1d00ba58
Judy OBrien <judyobrienie@gmail.com>
2016-09-24 16:09:41 +0100

    Test Case Added

---
+++ test/models/Fixtures.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Fixtures {
+	
+	 public static User[] users =
+		  {
+				  
+		    new User ("marge", "simpson", "marge@simpson.com",  "secret"),
+		    new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
+		    new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
+		    new User ("maggie","simpson", "maggie@simpson.com", "secret"),
+		    new User ("homer", "simpson", "homer@simpson.com",  "secret"),
+		  };
+
+}
