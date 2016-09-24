74b84e627508c1c18cacd9e391328ceb42127775
Judy OBrien <judyobrienie@gmail.com>
2016-09-24 15:51:56 +0100

    use IDs in addition to emails when referencing out users. introduce a separate collection call emailIndex - which stores the same object against a different key for rapid access:

--- src/models/User.java
+++ src/models/User.java
@@ -9,6 +9,9 @@ public class User
   public String lastName;
   public String email;
   public String password;
+  
+  static Long   counter = 0l;
+  public Long   id;
 
   public User()
   {
@@ -20,6 +23,8 @@ public class User
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+    this.id        = counter++;
+    
   }
   
   @Override
